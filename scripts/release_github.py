8682e66cbeb1f60a978376504d96baf55e185e95
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-01-04 20:15:31 -0500

    Last changes

--- scripts/release_github.py
+++ scripts/release_github.py
@@ -1,13 +1,18 @@
 """Release GitHub."""
+import pathlib
 import webbrowser
 from urllib.parse import urlencode
-import tomli
+
 import pkg_resources  # type: ignore[import]
-import pathlib
+import tomli
 
 
 def main() -> None:
-    """Open a tab ready to review and approve for new release."""
+    """
+    Open a tab ready to review and approve for new release.
+
+    Based on https://github.com/pypa/hatch/blob/master/scripts/release_github.py
+    """
     pkg_data = tomli.loads(pathlib.Path("pyproject.toml").read_text(encoding="utf-8"))
     version = pkg_resources.get_distribution("whats_this_payload").version
 
